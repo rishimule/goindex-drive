4227b689079338a02ca210450bb1dc0ef6f43685
Radon Gas <radonin123@protonmail.com>
2021-02-18 19:38:53 +0530

    pdf.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -368,22 +368,56 @@ function file_pdf(path){
   var tempurl = decodeURI(url)
   tempurl = decodeURI(tempurl)
   var dashurl = encodeURI(tempurl);
+  var url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf';
+
+  // Loaded via <script> tag, create shortcut to access PDF.js exports.
+  var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+  // The workerSrc property shall be specified.
+  pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+  // Asynchronous download of PDF
+  var loadingTask = pdfjsLib.getDocument(url);
+  loadingTask.promise.then(function(pdf) {
+    console.log('PDF loaded');
+
+    // Fetch the first page
+    var pageNumber = 1;
+    pdf.getPage(pageNumber).then(function(page) {
+      console.log('Page loaded');
+
+      var scale = 1.5;
+      var viewport = page.getViewport({scale: scale});
+
+      // Prepare canvas using PDF page dimensions
+      var canvas = document.getElementById('the-canvas');
+      var context = canvas.getContext('2d');
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+
+      // Render PDF page into canvas context
+      var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+      };
+      var renderTask = page.render(renderContext);
+      renderTask.promise.then(function () {
+        console.log('Page rendered');
+      });
+    });
+  }, function (reason) {
+    // PDF loading error
+    console.error(reason);
+  });
+
   var content = `
 <div class="mdui-container-fluid">
 	<br>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.7.570/pdf.min.js" integrity="sha512-g4FwCPWM/fZB1Eie86ZwKjOP+yBIxSBM/b2gQAiSVqCgkyvZ0XxYPDEcN2qqaKKEvK6a05+IPL1raO96RrhYDQ==" crossorigin="anonymous"></script>
+  <script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
 
-  <div class="container">
-  <object
-  data="${dashurl}#page=2"
-  type="application/pdf"
-  width="100%"
-  height="100%">
-  <p>Your browser does not support PDFs.
-    <a href="${dashurl}">Download the PDF</a>.</p>
-  </object>
-  </div>
+  <h1>PDF.js 'Hello, world!' example</h1>
 
+  <canvas id="the-canvas"></canvas>
 	<br>
 	<!-Fixed label->
 	<div class="mdui-textfield">
