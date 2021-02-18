b1f924a6b6bf5d06ce47caca7002d20b5ea7e37c
Radon Gas <radonin123@protonmail.com>
2021-02-18 21:24:54 +0530

    pdfss

--- themes/material/app.js
+++ themes/material/app.js
@@ -368,64 +368,24 @@ function file_pdf(path){
   var tempurl = decodeURI(url)
   tempurl = decodeURI(tempurl)
   var dashurl = encodeURI(tempurl);
-  var url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf';
-
-  // Loaded via <script> tag, create shortcut to access PDF.js exports.
-  var pdfjsLib = window['pdfjs-dist/build/pdf'];
-
-  // The workerSrc property shall be specified.
-  pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-
-  // Asynchronous download of PDF
-  var loadingTask = pdfjsLib.getDocument(url);
-  loadingTask.promise.then(function(pdf) {
-    console.log('PDF loaded');
-
-    // Fetch the first page
-    var pageNumber = 1;
-    pdf.getPage(pageNumber).then(function(page) {
-      console.log('Page loaded');
-
-      var scale = 1.5;
-      var viewport = page.getViewport({scale: scale});
-
-      // Prepare canvas using PDF page dimensions
-      var canvas = document.getElementById('the-canvas');
-      var context = canvas.getContext('2d');
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-
-      // Render PDF page into canvas context
-      var renderContext = {
-        canvasContext: context,
-        viewport: viewport
-      };
-      var renderTask = page.render(renderContext);
-      renderTask.promise.then(function () {
-        console.log('Page rendered');
-      });
-    });
-  }, function (reason) {
-    // PDF loading error
-    console.error(reason);
-  });
-
+  document.body.innerHTML = '';
   var content = `
-	<br>
-  <script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
+  <object
+    data="${dashurl}"
+    type="application/pdf"
+    width="100%"
+    height="100%">
+    <iframe
+      src="${dashurl}"
+      width="100%"
+      height="100%"
+      style="border: none;">
+      <p>Your browser does not support PDFs.
+        <a href="${dashurl}">Download the PDF</a>.</p>
+    </iframe>
+  </object>
 
-  <h1>PDF.js 'Hello, world!' example</h1>
-
-  <canvas id="the-canvas"></canvas>
-	<br>
-	<!-Fixed label->
-	<div class="mdui-textfield">
-	  <label class="mdui-textfield-label">Download link</label>
-	  <input class="mdui-textfield-input" type="text" value="${dashurl}"/>
-	</div>
-
-<a href="${dashurl}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
-	`;
+`;
 	$('#content').html(content);
 }
 
